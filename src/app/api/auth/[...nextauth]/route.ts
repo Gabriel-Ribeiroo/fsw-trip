09493c0bdb6929626600c491324b15dffa548f97
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-07-09 17:24:34 -0300

    fix: next auth error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { AuthOptions } from 'next-auth'
 import { Adapter } from 'next-auth/adapters'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
@@ -6,12 +6,16 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma) as Adapter,
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     })
   ]
-})
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
