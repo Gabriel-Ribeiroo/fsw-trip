5bc691f40a0d125d1cf8bc0af8bf2675aed57ca7
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-07-18 16:08:21 -0300

    feat: redirect user when logo is clicked

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import { useState } from 'react'
 
 import Menu from './Menu'
@@ -18,9 +19,11 @@ export default function Header() {
 	
 	return (
 		<header className="flex justify-between items-center px-4 gap-2 h-20 container mx-auto">
-			<div className="relative w-[11.563rem] h-[2.188rem]">
-				<Image src="/logo.png" alt="FSW Trip Logo" className="cursor-pointer" fill />
-			</div>
+			<Link href="/">
+				<div className="relative w-[11.563rem] h-[2.188rem]">
+					<Image src="/logo.png" alt="FSW Trip Logo" className="cursor-pointer" fill />
+				</div>
+			</Link>
 
 			{status === 'unauthenticated' && (
 				<button 
