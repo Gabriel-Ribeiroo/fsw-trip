8dbf47e0fd469de7d20a9397f81de494664360f3
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-07-26 11:10:19 -0300

    refactor: change logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Footer() {
 	return (
 		<footer className="flex flex-col gap-2 items-center p-4 bg-gray-100">
-			<Image src="/logo.png" alt="FSW Trip Logo" width={135} height={25} />
+			<Image src="/logo.png" alt="FSW Trip Logo" width={32} height={32} />
 			<p className="text-sm font-medium text-primary-darker">Todos os direitos reservados</p>
 		</footer>
 	)
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
 	return (
 		<header className="flex justify-between items-center px-4 gap-2 h-20 container mx-auto">
 			<Link href="/">
-				<div className="relative w-[11.563rem] h-[2.188rem]">
+				<div className="relative w-8 h-8">
 					<Image src="/logo.png" alt="FSW Trip Logo" className="cursor-pointer" fill />
 				</div>
 			</Link>
