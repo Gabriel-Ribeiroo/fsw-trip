2a9b1f4376b92563d3c4db4a8ddc362ea1128c37
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-07-31 15:47:37 -0300

    feat: add variants to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,32 @@
 'use client'
 
 import { ButtonHTMLAttributes, ReactNode } from 'react'
-import { twMerge } from 'tailwind-merge'
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+import { twMerge } from 'tailwind-merge'
+import { tv, type VariantProps } from 'tailwind-variants'
+
+const button = tv({
+	base: 'rounded-lg py-1.5 px-2 font-medium',
+	variants: {
+		variant: {
+			default: 'bg-primary text-white',
+			destructive: 'text-white bg-red-500',
+			defaultOutline: 'text-primary border border-primary',
+			destructiveOutline: 'border border-red-500 text-red-500',
+		}
+	}
+})
+
+type ButtonVariants = VariantProps<typeof button> 
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {
 	children: ReactNode 
 }
 
-export default function Button({ children, className, ...rest }: Props) {
+export default function Button({ children, className, variant = 'default', ...rest }: Props) {
 	return (
 		<button
-			className={twMerge('rounded-lg bg-primary text-white py-1.5 px-2 font-medium', className)}
+			className={twMerge(button({ variant }), className)}
 			{...rest}
 		>
 			{children}
