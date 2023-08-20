d0f2e8f00e90b515041d5f3483e544de0323592e
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-08-20 17:25:08 -0300

    style: add some styles to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,14 @@ import Menu from './Menu'
 
 export default function Header() {
 	return (
-		<header className="flex justify-between items-center px-4 gap-2 h-20 container mx-auto">
+		<header className="flex justify-between items-center px-4 gap-2 h-20 border-b border-gray-400">
 			<Link href="/">
 				<div className="relative w-8 h-8">
 					<Image src="/logo.png" alt="FSW Trip Logo" className="cursor-pointer" fill />
 				</div>
 			</Link>
 
-		 <Menu />
+		 	<Menu />
 		</header>
 	)
 }
