5e0fc346811660f749a4ef9c3836fad9f7bb1ece
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-08-26 00:55:00 -0300

    chore: add location description latitude and longitude to trip table

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['lh3.googleusercontent.com', 'images.unsplash.com']
+		domains: ['lh3.googleusercontent.com', 'images.unsplash.com', 'cf.bstatic.com', 'static51.com-hotel.com', 'q-xx.bstatic.com', 'dynamic-media-cdn.tripadvisor.com']
 	}
 }
 
