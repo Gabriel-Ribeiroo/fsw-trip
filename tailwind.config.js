f7a08b10938010cff9ca445a70e4328e4fdabb43
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-08-29 17:55:03 -0300

    feat: add shadcn-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
   theme: {
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+
 			backgroundImage: {
 				'search-area': 'url(/world-map.png)'
 			},
@@ -20,7 +37,7 @@ module.exports = {
 			textColor: {
 				dark: '#717171'
 			}
-		}
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
