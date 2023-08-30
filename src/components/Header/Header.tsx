20b1b6375f3040b43bf31c4f836cdf412a0f4408
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-08-30 16:24:56 -0300

    feat: add Dropdown Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Menu from './Menu'
 
 export default function Header() {
 	return (
-		<header className="flex justify-between items-center px-4 gap-2 h-20 border-b border-gray-400">
+		<header className="flex justify-between items-center px-8 gap-2 h-20 border-b border-gray-400">
 			<Link href="/">
 				<div className="relative w-8 h-8">
 					<Image src="/logo.svg" alt="FSW Trip Logo" className="cursor-pointer" fill />
--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -2,100 +2,59 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState, useRef, useEffect } from 'react'
 
 import { signOut, useSession } from 'next-auth/react'
 
+import * as Dropdown from '@/components/ui/DropdownMenu'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { HiOutlineUserCircle } from 'react-icons/hi'
-import { twMerge } from 'tailwind-merge'
 
 export default function Menu() {
-	const [isOpen, setIsOpen] = useState(false)
-	const dropdownRef = useRef<HTMLDivElement>(null)
-
 	const { status, data } = useSession()
-	
-	const handleMenuClick = () => setIsOpen(prevState => !prevState)
-	
-	const handleCloseDropdown = (event: MouseEvent) => {
-		if (dropdownRef.current !== event.target) 
-			setIsOpen(false)
-	}
 
-	useEffect(() => {
-		if (isOpen) 
-			document.addEventListener('click', handleCloseDropdown) 
-		
-		return () => document.removeEventListener('click', handleCloseDropdown)
-	}, [isOpen])
-	
 	return (
-		<div
-			onClick={handleMenuClick}
-			className="flex gap-3 items-center relative p-1.5 border-2 border-gray-500 rounded-3xl cursor-pointer"
-		>
-			<AiOutlineMenu size={18} />
+		<Dropdown.Root>
+			<Dropdown.Trigger>
+				<div
+					className="flex gap-3 items-center relative p-1.5 border-2 border-gray-500 rounded-3xl"
+				>
+					<AiOutlineMenu size={18} />
 
-			{status === 'authenticated' && (
-				<Image 
-					src={data!.user!.image!} 
-					alt={data!.user!.name!} 
-					width={30} 
-					height={30} 
-					className="rounded-full select-none" 
-				/>
-			)}
+					{status === 'authenticated' && (
+						<Image 
+							src={data!.user!.image!} 
+							alt={data!.user!.name!} 
+							width={30} 
+							height={30} 
+							className="rounded-full select-none" 
+						/>
+					)}
 
-			{['unauthenticated', 'loading'].includes(status) && (
-				<HiOutlineUserCircle size={30} className="rounded-full" />
-			)}
+					{['unauthenticated', 'loading'].includes(status) && (
+						<HiOutlineUserCircle size={30} className="rounded-full" />
+					)}
+				</div>
+			</Dropdown.Trigger>
 
 			{status !== 'loading' && (
-				<nav 
-					ref={dropdownRef}
-					onClick={(event) => event.stopPropagation()}
-					className={twMerge(
-						`absolute top-12 -left-32 shadow rounded-md w-48
-						text-sm font-semibold transition-all duration-75
-						shadow-zinc-400 p-1.5 text-primary bg-white z-50`,
-						isOpen ? 'scale-100 opacity-100' : 'scale-0 opacity-0'
-					)}
-				>
-					<ul className="flex flex-col">
-						<li>
-							{status === 'authenticated' && (
-								<button 
-									onClick={() => signOut()}
-									className="w-full text-start p-1 hover:bg-gray-200 border-radius rounded"
-								>
-									Logout
-								</button>
-							)}
-
-							{status === 'unauthenticated' && (
-								<Link 
-									href="/login" 
-									className="block w-full text-start p-1 hover:bg-gray-200 border-radius rounded" 
-								>
-									Login
-								</Link>							
-							)}
-						</li>
-
+				<Dropdown.Content>
+					<Dropdown.Item>
 						{status === 'authenticated' && (
-							<li>
-								<Link 
-									href="/my-trips" 
-									className="w-full block p-1 border-radius rounded hover:bg-gray-200"
-								>
-									Minhas viagens
-								</Link>
-							</li>
+							<button onClick={() => signOut()} className="flex-1 text-start">Logout</button>
 						)}
-					</ul>
-				</nav>
+					
+						{status === 'unauthenticated' && (
+							<Link href="/login" className="flex-1">Login</Link>
+						)}
+					</Dropdown.Item>
+
+				{status === 'authenticated' && (
+					<Dropdown.Item>
+						<Link href="my-trips">Minhas viagens</Link>
+					</Dropdown.Item>
+				)}
+			</Dropdown.Content>
 			)}
-		</div>
+		</Dropdown.Root>			
 	)
 }
