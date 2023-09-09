60cde58e18d573f04478c57be1fc99db1e265a73
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-09-09 11:01:28 -0300

    refactor: change confirmation page from client component to server component

---
+++ src/app/trips/[id]/confirmation/_components/CheckoutButton.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import Button from '@/components/Button'
+
+import { Trip } from '@prisma/client'
+import { toast } from 'react-toastify'
+import { loadStripe } from '@stripe/stripe-js'
+import { useSession } from 'next-auth/react'
+
+interface Props {
+	trip: Trip
+	startDate: string 
+	endDate: string 
+	guest: string 
+	totalPaid: string  
+}
+
+export default function CheckoutButton({ trip, startDate, endDate, guest, totalPaid }: Props) {
+	const { status } = useSession()
+
+	const router = useRouter() 
+	
+	const handleBuyClick = async () => {		
+		if (status !== 'authenticated')
+			return router.push('/login')
+
+		const request = await fetch('http://localhost:3000/api/payment', {
+			method: 'POST',
+			body: JSON.stringify({
+				tripId: trip.id,  
+				name: trip.name,
+				coverImage: trip.coverImage,
+				description: trip.description,
+				startDate: new Date(startDate), 
+				endDate: new Date(endDate),
+				totalPaid: Number(totalPaid),
+				guest: Number(guest),
+			})
+		})
+
+		if (!request.ok)
+			return toast.error('Ocorreu um erro ao realizar a reserva!', { position: 'bottom-center' })
+
+		const { sessionId } = await request.json()
+
+		const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!)
+
+		await stripe?.redirectToCheckout({ sessionId })
+	}
+	
+	return (
+		<Button 
+			className="mt-1" 
+			onClick={handleBuyClick}
+		>
+			Finalizar Compra
+		</Button>
+	)
+}
--- src/app/trips/[id]/confirmation/page.tsx
+++ src/app/trips/[id]/confirmation/page.tsx
@@ -1,122 +1,76 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
-import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
-
-import Button from '@/components/Button'
 import Card from './_components/Card'
+import CheckoutButton from './_components/CheckoutButton'
 
-import { useSession } from 'next-auth/react'
 import { format } from 'date-fns'
-import { Trip } from '@prisma/client'
-import { toast } from 'react-toastify'
-import { loadStripe } from '@stripe/stripe-js'
+import { redirect } from 'next/navigation'
 import ptBR from 'date-fns/locale/pt-BR'
 
 interface Props {
 	params: {
 		id: string 
 	}
+	searchParams: {
+		startDate: string 
+		endDate: string 
+		guests: string 
+	}
 }
 
-export default function TripConfirmation({ params: { id } }: Props) {
-	const [trip, setTrip] = useState<Trip | null>(null)
-	const [totalPaid, setTotalPaid] = useState<number>(0)
-
-	const router = useRouter()
-
-	const { status } = useSession()
-
-	const searchParams = useSearchParams()
-
-	const startDate = new Date(searchParams.get('startDate') as string)
-	const endDate = new Date(searchParams.get('endDate') as string) 
-	const guest = Number(searchParams.get('guests') as string)
-
-	const handleBuyClick = async () => {		
-		const request = await fetch('http://localhost:3000/api/payment', {
-			method: 'POST',
-			body: JSON.stringify({
-				tripId: id,  
-				startDate, 
-				endDate,
-				guest,
-				coverImage: trip?.coverImage,
-				totalPaid,
-				name: trip?.name,
-				description: trip?.description,
-			})
+async function getTrip(tripId: string, guest: string, startDate: string, endDate: string) {
+	const request = await fetch('http://localhost:3000/api/trip/check', {
+		method: 'POST', 
+		body: JSON.stringify({
+			tripId, 
+			guest: Number(guest),
+			startDate: new Date(startDate), 
+			endDate: new Date(endDate) 
 		})
+	}) 
 
-		if (!request.ok)
-			return toast.error('Ocorreu um erro ao realizar a reserva!', { position: 'bottom-center' })
+	return await request.json()
+}
 
-		const { sessionId } = await request.json()
+export default async function TripConfirmation({ 
+	params: { id }, 
+	searchParams: { startDate, endDate, guests } 
+}: Props) {
+	const response = await getTrip(id, guests, startDate, endDate)
 
-		const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!)
-
-		await stripe?.redirectToCheckout({ sessionId })
-
-		toast.success('Reserva realizada com sucesso!', { position: 'bottom-center' })
-	}
-
-	useEffect(() => {
-		if (status === 'unauthenticated')
-			router.push('/')
-	}, [status])
-
-	useEffect(() => {
-		const getTrip = async () => {
-			const request = await fetch('http://localhost:3000/api/trip/check', {
-				method: 'POST',
-				body: JSON.stringify({
-					tripId: id,
-					startDate,
-					endDate,
-					guest
-				})
-			})
-
-			const response = await request.json()
-
-			if (response.error) {
-				router.push('/')
-				toast.error(`${response.error}`)
-			}
-			
-			setTrip(response.trip)
-			setTotalPaid(response.totalPaid)
-		} 
-
-		getTrip()
-	}, [])
-
-	if(!trip) return null 
+	if (response.error) 
+		redirect('/')
 	
+	const trip = response.trip
+	const totalPaid = response.totalPaid
+
 	return (
 		<main className="flex flex-col gap-2 p-2 flex-1 w-full max-w-2xl mx-auto">
 			<h1 className="font-semibold text-xl font-primary-darker mt-2.5">Sua viagem</h1>
-
 			<Card trip={trip} totalPrice={totalPaid} />
 
 			<div className="flex flex-col gap-5 text-primary-darker">
 				<div>
 					<h3 className="font-semibold text-lg mt-2">Data</h3>
 					
-					<div className="flex">
-						<p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })} -</p>
-						<p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+					<div className="flex gap-1.5">
+						<p>{format(new Date(startDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+						<span>-</span>
+						<p>{format(new Date(endDate), "dd 'de' MMMM", { locale: ptBR })}</p>
 					</div>
 				</div>
 				
 				<div>
 					<h3 className="font-semibold text-lg">Hóspedes</h3>
-					<p>{guest} hóspedes</p>
+					<p>{guests} hóspedes</p>
 				</div>
 			</div>
 
-			<Button onClick={handleBuyClick}>Finalizar Compra</Button>
+			<CheckoutButton 
+				trip={trip} 
+				startDate={startDate} 
+				endDate={endDate} 
+				guest={guests}
+				totalPaid={totalPaid}
+			/>
 		</main>
 	)
 }
