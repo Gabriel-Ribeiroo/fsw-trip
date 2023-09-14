f087876f2410a9bca6eedf070efd618c3a68cb48
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-09-14 17:29:47 -0300

    feat: add credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,74 @@
+import bcrypt from 'bcrypt'
+import CredentialProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import type { Adapter } from 'next-auth/adapters'
 import type { NextAuthOptions } from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { prisma } from './prisma'
+import { prisma } from '@/lib/prisma'
 
 export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
-    GoogleProvider({
+		CredentialProvider({
+			name: 'credentials',
+			credentials: {
+				email: { label: 'password', type: 'text', placeholder: 'email' },
+				username: { label: 'username', type: 'text', placeholder: 'username' },
+				password: { label: 'password', type: 'password', placeholder: 'password' }
+			},
+			async authorize(credentials): Promise<any> {
+				if (!credentials?.email || !credentials?.password) 
+					throw new Error('Dados de login necessarios')
+	
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email
+					}
+				})
+
+				if (!user || !user.hashedPassword) 
+					throw new Error('Usúario não registrado com credenciais') 
+				
+				const matchPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+				if (!matchPassword)
+					throw new Error('Senha incorreta') 
+				 
+				return user  
+			}
+		}),
+		GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    })
+    }),
   ],
+	session: {
+		strategy: 'jwt'
+	},
 	secret: process.env.NEXTAUTH_SECRET,
 	callbacks: {
-		async session({ session, user }) {
-			session.user = { ...session.user, id: user.id } as { id: string; name: string; email: string }
-			return session 
+		async jwt({ token, user }) {
+			if (user) {
+				return {
+					...token, 
+					id: user.id, 
+				}
+			}
+
+			return token 
+		},
+		async session({ session, token }) {
+			return {
+				...session, 
+				user: {
+					...session.user, 
+					id: token.id 
+				}
+			} 
 		}
 	},
 	pages: {
 		signIn: '/login'
 	},
+	debug: process.env.NODE_ENV === 'development'
 }
