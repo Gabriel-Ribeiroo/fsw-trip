f2836f69fea73532f76038a31d948f5a44678516
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-09-16 20:17:16 -0300

    feat: handle errors on trip reservation deletion

--- src/app/api/trip/reservation/[id]/route.ts
+++ src/app/api/trip/reservation/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
 
 interface Props {
 	params: {
@@ -9,11 +10,22 @@ interface Props {
 }
 
 export async function DELETE(req: NextRequest, { params: { id } }: Props) {
-	const reservation = await prisma.tripReservation.delete({
-		where: {
-			id 
-		}
-	})
+	try {
+		const reservation = await prisma.tripReservation.delete({
+			where: {
+				id
+			}
+		})
 
-	return NextResponse.json(reservation)
+		return NextResponse.json({ error: false, reservation })
+	} 
+	
+	catch (error) {
+		if (error instanceof Prisma.PrismaClientKnownRequestError) {
+			return NextResponse.json({ 
+				error: true, 
+				message: `Aconteceu um erro ao tentar cancelar a reserva. Por favor, tente novamente.` 
+			})
+		}
+	}
 }
