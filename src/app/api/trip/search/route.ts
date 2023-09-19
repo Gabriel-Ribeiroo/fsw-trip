2ff319c07a69f30486b3c159e2846332eaac091b
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-09-19 11:38:49 -0300

    refactor: improve generate query params function

--- src/app/api/trip/search/route.ts
+++ src/app/api/trip/search/route.ts
@@ -1,44 +1,31 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 import { prisma } from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
 
 function generateQueryParams
 (location: string | undefined, initialDate: string | undefined, budget: string | undefined) {	
 	const normalizedLocation = location && location.replace(/ /g, '&')
 	
-	let queryParams: any = {
+	const queryParams: any = {
 		AND: [], 
 		OR:  []
 	}
 
-	if (location)
-		queryParams = {
-			...queryParams, 
-			OR: [
-				{ name: { search: normalizedLocation } }, 
-				{ description: { search: normalizedLocation } }, 
-				{ location: { search: normalizedLocation } }
-			]
-		}
-
+	if (location) {
+		queryParams.OR.push(
+			{ name: { search: normalizedLocation } },
+			{ description: { search: normalizedLocation } },
+			{ location: { search: normalizedLocation } }
+		)	
+	}
+	
 	if (initialDate) {
-		queryParams = {
-			...queryParams,
-			AND: [
-				...queryParams.AND,
-				{startDate: { gte: new Date(initialDate) } }
-			]
-		}
+		queryParams.AND.push({ startDate: { gte: new Date(initialDate) } })
 	}
 	
 	if (budget) {
-		queryParams = {
-			...queryParams, 
-			AND: [
-				...queryParams.AND,
-				{ pricePerDay: { lte: Number(budget) } }
-			]
-		}
+		queryParams.AND.push({ pricePerDay: { lte: Number(budget) } })
 	}
 	
 	return queryParams
