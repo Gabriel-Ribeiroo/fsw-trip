8840e084361b07e69fa6b9bf582b06ebf33a2a2c
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-09-22 13:49:41 -0300

    feat: improve reservation schema

--- src/schemas/reservation.ts
+++ src/schemas/reservation.ts
@@ -1,25 +1,46 @@
 import { z } from 'zod'
 
-export default function createDynamicSchema(maxGuests: number) {
-	const schema = z
-		.object({
-			startDate: z.date({ required_error: 'Campo obrigatório.', invalid_type_error: 'Informe uma data válida.' }),
-
-			endDate: z.date({ required_error: 'Campo obrigatório.', invalid_type_error: 'Informe uma data válida.' }),
-
-			guests: z.number({ invalid_type_error: 'Campo obrigatório.' })
-				.min(1, { message: 'É necessário ao menos um hóspede.' })
-				.max(maxGuests, { message: `Limite de convidados excedido.` })
-		})
-		.refine(data => data.startDate < data.endDate, { 
-			message: 'Data inicial maior do que final.', path: ['startDate']  
-		})
-
-	return schema 
-}
-
 export interface Form {
 	startDate: Date
 	endDate: Date 
 	guests: number 
+}
+
+export interface SchemaValidationError {
+	startDate?: string[]
+	endDate?: string[] 
+	guests?: string[]
+}
+
+export default function createDynamicSchema(
+	maxGuests: number, 
+	tripStartDate: Date, 
+	tripEndDate: Date
+) {
+	return z
+		.object({
+			startDate: z.date({ 
+				required_error: 'Campo obrigatório.', 
+				invalid_type_error: 'Informe uma data válida.' 
+			})
+				.min(tripStartDate, { message: 'Data inválida.' })
+				.max(tripEndDate, { message: 'Data inválida.' }),
+
+			endDate: z.date({ 
+				required_error: 'Campo obrigatório.', 
+				invalid_type_error: 'Informe uma data válida.' 
+			})
+				.min(tripStartDate, { message: 'Data inválida.' })
+				.max(tripEndDate, { message: 'Data inválida.' }),
+
+			guests: z.number({ 
+				invalid_type_error: 'Campo obrigatório.' 
+			})
+				.min(1, { message: 'É necessário ao menos um hóspede.' })
+				.max(maxGuests, { message: `Limite de convidados excedido.` })
+		})
+		.refine((data) => 
+			new Date(data.startDate) <= new Date(data.endDate),
+			{ message: 'Data inválida.', path: ['endDate'] }
+		)
 }
